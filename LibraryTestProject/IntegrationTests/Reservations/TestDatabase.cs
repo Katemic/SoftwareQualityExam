5eca0435512781e8d96608ea9c974e5f1e2a1968
Cyperf <drageon@live.dk>
2026-05-30 16:57:53 +0200

    First integration tests (still needs to manually reset db) (need to add more)

---
+++ LibraryTestProject/IntegrationTests/Reservations/TestDatabase.cs
@@ -0,0 +1,21 @@
+﻿using LibrarySQLBackend.Context;
+using LibraryTestProject.IntegrationTests.Reservations;
+using Microsoft.EntityFrameworkCore;
+
+public static class TestDatabase
+{
+    private const string ConnectionString =
+        "Server=localhost;Port=3309;Database=mydb;User=root;Password=mysecretpassword;";
+
+    public static async Task<AppDbContext> CreateFreshDatabase()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseMySql(
+                ConnectionString,
+                ServerVersion.AutoDetect(ConnectionString))
+            .Options;
+
+        var context = new AppDbContext(options);
+        return context;
+    }
+}
