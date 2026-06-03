8bc698ff71796ed69c917dd47827fefe7481a461
Katrine <katrinemichala@gmail.com>
2026-06-03 12:04:48 +0200

    fixed unit test

--- LibraryTestProject/UnitTests/LoanUnitTests.cs
+++ LibraryTestProject/UnitTests/LoanUnitTests.cs
@@ -228,7 +228,7 @@ public class LoanUnitTests
         loanRepositoryMock
             .Setup(x => x.HasOverdueLoanAsync(
                 dto.LoanerId))
-            .ReturnsAsync(false); //make true again
+            .ReturnsAsync(true);
 
         // Act + Assert
         await Assert.ThrowsExceptionAsync<InvalidOperationException>(
